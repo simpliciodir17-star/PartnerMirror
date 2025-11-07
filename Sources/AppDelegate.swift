376e5b3390db355f73c9d92f00534dd6cabc52b3
simpliciodir17-star <simplicio.dir17@gmail.com>
2025-11-07 17:23:33 -0300

    Update AppDelegate.swift

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -8,9 +8,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return true
   }
 
+  // iOS 13+ usa SceneDelegate
   func application(_ application: UIApplication,
                    configurationForConnecting connectingSceneSession: UISceneSession,
                    options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    let cfg = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    cfg.delegateClass = SceneDelegate.self
+    return cfg
   }
 }
