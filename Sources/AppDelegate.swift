f2ee1dbc9cc9c56f3aabfc368c4cee7e2cc0ba4a
simpliciodir17-star <simplicio.dir17@gmail.com>
2025-11-07 20:16:20 -0300

    Update AppDelegate.swift

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -1,28 +1,36 @@
-import UIKit
-
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-  var window: UIWindow?
-
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    if #available(iOS 13.0, *) {
-      // A janela será criada pelo SceneDelegate nas versões que utilizam Scenes.
-    } else {
-      let window = UIWindow(frame: UIScreen.main.bounds)
-      window.rootViewController = WebViewController()
-      window.backgroundColor = .white
-      window.makeKeyAndVisible()
-      self.window = window
-    }
-    return true
-  }
-
-  // iOS 13+ usa SceneDelegate
-  func application(_ application: UIApplication,
-                   configurationForConnecting connectingSceneSession: UISceneSession,
-                   options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-  }
-}
+diff --git a/Sources/AppDelegate.swift b/Sources/AppDelegate.swift
+index ea43e0ae3c737755830738acd0f3d0bdcadc820d..a19bdc5a2bb1957249194d05323ca5d0b33c0e35 100644
+--- a/Sources/AppDelegate.swift
++++ b/Sources/AppDelegate.swift
+@@ -1,17 +1,30 @@
+ import UIKit
+ 
+ @main
+ class AppDelegate: UIResponder, UIApplicationDelegate {
+ 
++  var window: UIWindow?
++
+   func application(_ application: UIApplication,
+                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
++    if #available(iOS 13.0, *) {
++      // A janela será criada pelo SceneDelegate nas versões que utilizam Scenes.
++    } else {
++      let window = UIWindow(frame: UIScreen.main.bounds)
++      window.rootViewController = WebViewController()
++      window.backgroundColor = .white
++      window.makeKeyAndVisible()
++      self.window = window
++    }
+     return true
+   }
+ 
+   // iOS 13+ usa SceneDelegate
+   func application(_ application: UIApplication,
+                    configurationForConnecting connectingSceneSession: UISceneSession,
+                    options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+-    UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
++    let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
++    configuration.delegateClass = SceneDelegate.self
++    return configuration
+   }
+ }
