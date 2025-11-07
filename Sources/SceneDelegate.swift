a87bacd75edf19fb20e8a03a857cb30c3672abb0
simpliciodir17-star <simplicio.dir17@gmail.com>
2025-11-07 17:23:45 -0300

    Update SceneDelegate.swift

--- Sources/SceneDelegate.swift
+++ Sources/SceneDelegate.swift
@@ -3,11 +3,16 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    guard let ws = scene as? UIWindowScene else { return }
-    let window = UIWindow(windowScene: ws)
-    window.rootViewController = WebViewController()
-    self.window = window
-    window.makeKeyAndVisible()
+  func scene(_ scene: UIScene,
+             willConnectTo session: UISceneSession,
+             options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+
+    let win = UIWindow(windowScene: windowScene)
+    // 👉 força o WebViewController como tela inicial
+    win.rootViewController = WebViewController()
+    win.backgroundColor = .white
+    win.makeKeyAndVisible()
+    self.window = win
   }
 }
