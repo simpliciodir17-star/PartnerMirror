0a79e152f07e605e788f101629aef1522c988d36
simpliciodir17-star <simplicio.dir17@gmail.com>
2025-11-07 20:09:14 -0300

    Update AppDelegate.swift

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -3,8 +3,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  var window: UIWindow?
+
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    if #available(iOS 13.0, *) {
+      // A janela será criada pelo SceneDelegate nas versões que utilizam Scenes.
+    } else {
+      let window = UIWindow(frame: UIScreen.main.bounds)
+      window.rootViewController = WebViewController()
+      window.backgroundColor = .white
+      window.makeKeyAndVisible()
+      self.window = window
+    }
     return true
   }
 
