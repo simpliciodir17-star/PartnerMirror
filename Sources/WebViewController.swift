89f0f608f1e890e43067c3eea8cdbb56acb6a34f
simpliciodir17-star <simplicio.dir17@gmail.com>
2025-11-07 16:17:35 -0300

    Update WebViewController.swift

--- Sources/WebViewController.swift
+++ Sources/WebViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import WebKit
 
-final class WebViewController: UIViewController, WKNavigationDelegate {
+final class WebViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
   private var webView: WKWebView!
   private let partnerURL = URL(string: "https://partner.obynexbroker.com/partner")!
 
@@ -9,9 +9,41 @@ final class WebViewController: UIViewController, WKNavigationDelegate {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
 
+    let userContent = WKUserContentController()
+
+    let injectedJS = #"""
+    (function() {
+      function findToken() {
+        try {
+          var token = null;
+          token = window.localStorage.getItem('auth_token')
+               || window.localStorage.getItem('token')
+               || window.localStorage.getItem('accessToken')
+               || window.sessionStorage.getItem('auth_token')
+               || window.sessionStorage.getItem('token')
+               || window.sessionStorage.getItem('accessToken');
+          if (!token && window.__INITIAL_STATE__ && window.__INITIAL_STATE__.auth) token = window.__INITIAL_STATE__.auth.token;
+          if (!token) {
+            var m = document.cookie.match(/(?:^|; )(?:auth_token|token|accessToken)=([^;]+)/);
+            if (m) token = decodeURIComponent(m[1]);
+          }
+          if (token && window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.nativeHandler) {
+            window.webkit.messageHandlers.nativeHandler.postMessage({type:'auth_token', token: token});
+          }
+        } catch(e) { /* silent */ }
+      }
+      window.addEventListener('load', findToken);
+      setInterval(findToken, 3000);
+    })();
+    """#
+    let userScript = WKUserScript(source: injectedJS, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+    userContent.addUserScript(userScript)
+    userContent.add(self, name: "nativeHandler")
+
     let config = WKWebViewConfiguration()
-    config.websiteDataStore = .default()           // cookies/localStorage persistentes
-    config.preferences.javaScriptEnabled = true    // JS ligado
+    config.websiteDataStore = .default()
+    config.preferences.javaScriptEnabled = true
+    config.userContentController = userContent
 
     webView = WKWebView(frame: .zero, configuration: config)
     webView.navigationDelegate = self
@@ -28,4 +60,8 @@ final class WebViewController: UIViewController, WKNavigationDelegate {
     req.cachePolicy = .reloadIgnoringLocalCacheData
     webView.load(req)
   }
-}
+
+  func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+    guard message.name == "nativeHandler",
+          let body = message.body as? [String: Any],
+          (body["type"] as? Strin
