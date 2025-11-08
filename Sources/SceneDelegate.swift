5a51cce7eed3c9085d195b76a9e982c7c61ffc9c
simpliciodir17-star <simplicio.dir17@gmail.com>
2025-11-08 17:09:00 -0300

    Update SceneDelegate.swift

--- Sources/SceneDelegate.swift
+++ Sources/SceneDelegate.swift
@@ -6,8 +6,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene,
              willConnectTo session: UISceneSession,
              options connectionOptions: UIScene.ConnectionOptions) {
-    guard let windowScene = (scene as? UIWindowScene) else { return }
-    let win = UIWindow(windowScene: windowScene)
+    guard let ws = (scene as? UIWindowScene) else { return }
+    let win = UIWindow(windowScene: ws)
     win.rootViewController = WebViewController()
     win.backgroundColor = .systemBackground
     win.makeKeyAndVisible()
